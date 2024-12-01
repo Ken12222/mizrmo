cf14ded414f887a34274159224f709a2951b649d
Ken <ashartey2@gmail.com>
2024-12-01 20:53:59 +0000

    router fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="mizrmo">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     open: true, // Automatically open in the
-    host: true,
+    host: true, //Preview app on mobile
+    historyApiFallback: true, //handle routuing and
   },
 });
